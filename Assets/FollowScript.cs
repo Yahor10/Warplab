1bdb1630748a03155f13cb2e0901a10ebde608c2
Egor Chebotarev <T_EgorC1@verifone.com>
2015-05-25 15:06:12 +0300

    update layers

--- Assets/FollowScript.cs
+++ Assets/FollowScript.cs
@@ -14,6 +14,8 @@ public class FollowScript : MonoBehaviour {
 	void Start() {
 		bar = GameObject.Find("Character").transform;
 		startPoint = transform.position;
+		Debug.Log ("bar" + bar.transform.position.y);
+	
 	}
 	private Vector3 target;
 
@@ -23,18 +25,30 @@ public class FollowScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
-		float xOffset = 2;
-		Debug.Log (" h" +  bar.localScale.x);
 
+	}
+
+	void LateUpdate(){
+		float xOffset = 2;
+		
 		if (bar.localScale.x > 0) {
 			xOffset = -2;
-			rightDir =true;
 		}
 		else{
-		    xOffset = 2;
-			rightDir = false;
+			xOffset = 2;
 		}
-	
-		transform.position = Vector3.Lerp(transform.position, bar.position + new Vector3(xOffset,-1,0), 0.3f);
+
+		//transform.position = Vector3.Lerp(transform.position, bar.position + new Vector3(xOffset,-1,0), 0.3f);
+
+		float f =  Mathf.Lerp( transform.position.x, bar.position.x + xOffset, Time.deltaTime * 1f);
+		transform.position = new Vector3(f,
+		                                                             transform.position.y,
+		                                                              transform.position.z);
+		Physics2D.IgnoreLayerCollision (10, 11);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{			
+
 	}
 }
