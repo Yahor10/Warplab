9e826ab7b7192dc567cec16f3625185676f34ba8
Egor Chebotarev <T_EgorC1@verifone.com>
2015-06-02 13:59:56 +0300

    update simple fly bot ai

--- Assets/WarpBots/FlyBotAi.cs
+++ Assets/WarpBots/FlyBotAi.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnitySampleAssets._2D;
 
 public class FlyBotAi : FlyBotBehaviour
 {
@@ -9,11 +10,26 @@ public class FlyBotAi : FlyBotBehaviour
 
 	private Transform character;
 
+	private float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+
+	float distToGround;
+	
+	[SerializeField] private LayerMask whatIsGround;
+
+	Transform groundCheck;
+
+ // A mask determining what is ground to the character
+
+	
 	// Use this for initialization
 	void Start ()
 	{
-		character = GameObject.Find("Character").transform;
+		character = GameObject.Find ("Character").transform;
 
+		InvokeRepeating("flyState",0.2f,0.2f);
+
+		groundCheck = character.Find("GroundCheck");
+		
 	}
 	
 	// Update is called once per frame
@@ -24,10 +40,6 @@ public class FlyBotAi : FlyBotBehaviour
 
 	void FixedUpdate () 
 	{
-		if (transform.position.y < 0) {
-			rigidbody2D.AddForce (new Vector2 (-1, flyForce));
-		}
-
 		switch (currentBehaviour) {
 		case Behavior.follow:
 			flyFollowState();
@@ -38,19 +50,33 @@ public class FlyBotAi : FlyBotBehaviour
 	bool upped = false; 
 
 	void  flyFollowState() {
-		float xOffset = 2;
+		float xOffset = 1;
 		
 		if (character.localScale.x > 0) {
-			xOffset = -2;
+			xOffset = -1;
 		}
 		else {
-			xOffset = 2;
+			xOffset = 1;
 		}
 		float f = Mathf.Lerp (transform.position.x, character.position.x + xOffset, Time.deltaTime * 1f);
 		transform.position = new Vector3 (f, transform.position.y, transform.position.z);
 
 		Physics2D.IgnoreLayerCollision (10, 11);
 	}
-	
+
+
+	void flyState(){
+
+		float dist = Vector2.Distance (transform.position, character.position);
+		Debug.Log ("Dist" + dist);
+		if (transform.position.y < character.position.y) {			
+			rigidbody2D.AddForce (new Vector2 (transform.position.x, flyForce));
+		}
+		if (transform.position.y >= character.position.y + 5) {
+			rigidbody2D.AddForce (new Vector2 (transform.position.x, -flyForce));
+		}
+	}
+
+
 }
 
