17e5a2cea3e0cca63326ec37975a34d6e20da47b
Egor Chebotarev <T_EgorC1@verifone.com>
2015-06-08 10:53:33 +0300

    update fly bot

--- Assets/WarpBots/FlyBotAi.cs
+++ Assets/WarpBots/FlyBotAi.cs
@@ -46,10 +46,26 @@ public class FlyBotAi : FlyBotBehaviour
 		
 	}
 
-
+	bool clicked = false;
+	
 	// Update is called once per frame
 	void Update ()
 	{
+
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			if(!clicked){
+				startVector = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+
+			}
+
+			clicked = true;
+			Debug.Log ("GetKeyDown");
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			clicked = false;
+		}
+	
 		Vector3 lookDirection = GameObject.Find("FlyEnemy").transform.position - flyBotWeapon.position;
 		float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
 		Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
@@ -128,17 +144,6 @@ public class FlyBotAi : FlyBotBehaviour
 		currentBehaviour = Behavior.attack;
 	}
 
-	public void setAttackTarget(Transform target){
-		if (target == null) {
-			Debug.LogError ("target attack is null!");
-			return;
-		}
-
-		attackTarget = target;
-		setAttackState ();
-
-	}
-
 	void OnCollisionEnter2D(Collision2D coll)
 	{			
 	}
@@ -155,8 +160,63 @@ public class FlyBotAi : FlyBotBehaviour
 
 	}
 
-
-
+	void DrawRectangle (Rect position, Color color)
+	{    
 	
+		// We shouldn't draw until we are told to do so.
+		if (Event.current.type != EventType.Repaint) {
+			// not repaint event
+			return;
+		}
+
+		// Please assign a material that is using position and color.
+		if (material == null) {
+			Debug.LogError ("You have forgot to set a material.");
+			return;
+		}
+
+		material.SetPass (0);
+		
+		// Optimization hint: 
+		// Consider Graphics.DrawMeshNow
+		GL.Color (color);
+		GL.Begin (GL.QUADS);
+		GL.Vertex3 (position.x, position.y, 0);
+		GL.Vertex3 (position.x + position.width, position.y, 0);
+		GL.Vertex3 (position.x + position.width, position.y + position.height, 0);
+		GL.Vertex3 (position.x, position.y + position.height, 0);
+		GL.End ();
+	}
+
+	Vector2 startVector;
+	Vector2 vd;
+	void OnGUI ()
+	{        
+		if(clicked){
+			vd = new Vector2(Input.mousePosition.x -startVector.x, Screen.height - Input.mousePosition.y - startVector.y);
+			Rect rect = new Rect (startVector.x,startVector.y,  vd.x, vd.y);
+
+			Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+			pos.y = Screen.height - pos.y;
+			Debug.Log("Rect" + rect.Contains(pos));
+
+			DrawRectangle ( rect, color);        
+		}	
+	}
+
+	// Please assign a material that is using position and color.
+	public Material material;
+
+	public Color color;
+
+
+	public void setAttackTarget(Transform target){
+		if (target == null) {
+			return;
+		}
+
+		attackTarget = target;
+		setAttackState ();
+	}
 }
 
