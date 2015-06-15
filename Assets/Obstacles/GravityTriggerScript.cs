7783f977d7c726524428d5a68b6d9fe5185619f5
Egor Chebotarev <T_EgorC1@verifone.com>
2015-06-15 10:36:25 +0300

    add obstacles folder

---
+++ Assets/Obstacles/GravityTriggerScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class GravityTriggerScript : MonoBehaviour
+{
+
+	// Use this for initialization
+
+	Transform character;
+	void Start ()
+	{
+		character = GameObject.Find ("Character").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		character.rigidbody2D.AddForce (new Vector2 (0, 20), ForceMode2D.Impulse);
+	}
+}
+
---
+++ Assets/Obstacles/WallElecticity.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallElecticity : MonoBehaviour {
+
+	// Use this for initialization
+
+	Color[] colors = {Color.blue,Color.black,Color.red,Color.green};
+	private SpriteRenderer render;
+	void Start () {	
+		render = GetComponent<SpriteRenderer> ();
+		StartCoroutine (WaitAndPrint (1.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	IEnumerator WaitAndPrint(float waitTime) {
+		while (true) { 
+			yield return new WaitForSeconds (waitTime);
+			print ("WaitAndPrint " + Time.time);
+			render.color = colors[Random.Range(0,3)];
+		}
+	}
+}
