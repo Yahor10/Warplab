f12be97d202aa62cb7c369fa3b98dc5a58fcf7e2
Egor Chebotarev <T_EgorC1@verifone.com>
2015-06-25 12:11:38 +0300

    bug fixing spawn

--- Assets/SampleAssets/2D/Scripts/CharacterStatus.cs
+++ Assets/SampleAssets/2D/Scripts/CharacterStatus.cs
@@ -5,14 +5,11 @@ public class CharacterStatus : MonoBehaviour {
 
 
 	public int HealthPoint = 20;
-
-	Vector3 startPosition;
-
+	
 	SpawnPlayer spawnScript;
 	// Use this for initialization
 	void Start () {		 
 		Debug.Log ("init char status");
-		startPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 		GameObject gameObject = GameObject.FindGameObjectWithTag ("GameMaster");
 		spawnScript = gameObject.GetComponent<SpawnPlayer> ();
 	}
@@ -40,6 +37,6 @@ public class CharacterStatus : MonoBehaviour {
 		yield return new WaitForSeconds(2);
 		Debug.Log ("respawn");
 		renderer.enabled = true;
-		spawnScript.reloadLevel (startPosition);
+		spawnScript.reloadLevel ();
 	}
 }
